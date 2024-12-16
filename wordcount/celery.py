68079ee7fca646b67103777bf4791b84c4725ee8
rohiem <rehimovich@gmail.com>
2024-12-16 10:25:20 +0200

    removing celery redis after testing then  scale

--- wordcount/celery.py
+++ wordcount/celery.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, unicode_literals
+""" from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
@@ -8,3 +8,4 @@ app = Celery('wordcount')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+ """
--- wordcount/settings.py
+++ wordcount/settings.py
@@ -74,13 +74,13 @@ WSGI_APPLICATION = 'wordcount.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-} """
+} 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -92,6 +92,7 @@ DATABASES = {
     }
 }
 
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
